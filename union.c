fea180b8f605ab0b4d00acd944b4ff790319203f
kalaiselvam-ganesan <114068214+kalaiselvam-ganesan@users.noreply.github.com>
2022-10-05 16:24:07 +0530

    Add files via upload

---
+++ union.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+union Data {
+   int i;
+   float f;
+   char str[20];
+};
+ 
+int main( ) {
+
+   union Data data;        
+
+   data.i = 10;
+   data.f = 220.5;
+   strcpy( data.str, "C Programming");
+
+   printf("\n %d %f %s",data.i,data.f,data.str);
+   
+   /* accessing the member */
+   printf( "data.i : %d\n", data.i);
+   printf( "data.f : %f\n", data.f);
+   printf( "data.str : %s\n", data.str);
+
+   return 0;
+}
