01c39ac0d455d766926b7064c4d4669d210cc80d
kalaiselvam-ganesan <114068214+kalaiselvam-ganesan@users.noreply.github.com>
2022-11-10 16:25:09 +0530

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,36 @@
+// Problem 1
+#include<stdio.h>
+int main()
+{
+  printf("%d Hello %d");                         // output
+  return 0;                                     //Garbage value  Hello garbage Value
+}
+
+
+// Problem 2
+#include<stdio.h>
+int main()
+{
+  printf("%d Hello %d");                        //output
+  return 0;                                    //0.00000 Hello 0.000000
+}
+
+
+// Problem 3
+#include<stdio.h>
+int main()
+{
+  printf("%s Friend %s");                       //output
+  return 0;                                    //Garbage value Friend  Garbage value
+}
+
+
+
+// Problem 4
+#include<stdio.h>
+int main()
+{
+  printf("%c Friend %c");                     //output
+  return 0;                                   //Garbage value Friend Garbage value
+}
+
